3ee17e7857121a93572b6882f8d1f0ad3d32ddde
Juanmi Taboada <juanmi@centrologic.com>
2017-08-30 09:03:25 +0200

    Better validation

--- codenerix_extensions/validators.py
+++ codenerix_extensions/validators.py
@@ -1,7 +1,10 @@
 import re
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
-def spanishNIFNIECIF(cid):
+
+def spanishNIFNIECIFhelper(cid):
     '''
     Given a spanish NIF, CIF or NIE, it returns:
 
@@ -11,7 +14,9 @@ def spanishNIFNIECIF(cid):
     -1 for an invalid NIF (it looks like NIF but it doesn't validate the CRC)
     -2 for an invalid CIF (it looks like CIF but it doesn't validate the CRC)
     -3 for an invalid NIE (it looks like NIE but id doesn't validate the CRC)
-    0 it can not recognize if it is NIF, CIF or NIE, conclusion: the given CID is incorrect.
+    -4 not a valid NIF/NIE/CIF because it is too long
+    -5 not a valid NIF/NIE/CIF because it doesn't have a valid format
+    -6 it can not recognize if it is NIF, CIF or NIE, conclusion: the given CID is incorrect.
     '''
 
     # Set upper case
@@ -20,13 +25,13 @@ def spanishNIFNIECIF(cid):
     # Already too long?
     if len(cid) > 9:
         # Incorrect CID
-        return 0
+        return -4
 
     # Check the format
     compiled = re.compile('((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)')
     if not compiled.match(cid):
         # It doesn't have a valid format, incorrect CID
-        return 0
+        return -5
 
     # Build a string with digits for NIF checks
     num = ''
@@ -59,7 +64,7 @@ def spanishNIFNIECIF(cid):
     n = 10 - int(str(suma)[-1])
 
     # Special NIFs (they are checked as CIFs)
-    compiled=re.compile('^[KLM]{1}')
+    compiled = re.compile('^[KLM]{1}')
     if compiled.match(cid):
         # It looks like a Special NIF
         if num[8] == chr(64 + n):
@@ -107,8 +112,111 @@ def spanishNIFNIECIF(cid):
             return -3
 
     # Default fall out is unrecogniced CID, so it is incorrect
-    return 0
+    return -6
+
+
+def spanishNIFNIECIF(nif=True, nie=True, cif=True, details=True):
+
+    def worker(cid, nif, nie, cif, details):
+        # Make analisys
+        kind = spanishNIFNIECIFhelper(cid)
+
+        # Prepare basic answers
+        if kind < 0:
+            if details:
+                if nif and kind == -1:
+                    raise ValidationError(_('NIF %(cid)s is misspelled'), params={'cid': cid})
+                elif cif and kind == -2:
+                    raise ValidationError(_('CIF %(cid)s is misspelled'), params={'cid': cid})
+                elif nie and kind == -3:
+                    raise ValidationError(_('NIE %(cid)s is misspelled'), params={'cid': cid})
+                elif kind == -4:
+                    raise ValidationError(_('CID %(cid)s is too long'), params={'cid': cid})
+                elif kind == -5:
+                    raise ValidationError(_('CID %(cid)s has a wrong format'), params={'cid': cid})
+                elif kind == -6:
+                    raise ValidationError(_('CID %(cid)s is incorrect'), params={'cid': cid})
+
+            # In any other case, it is incorrect
+            raise ValidationError(_('%(cid)s is incorrect'), params={'cid': cid})
+
+        # Check for valid CIDs which are not allowed
+        elif kind == 1 and not nif:
+            if details:
+                raise ValidationError(_('NIFs are not allowed in this field'))
+            else:
+                raise ValidationError(_('CID %(cid)s is incorrect'), params={'cid': cid})
+        elif kind == 2 and not cif:
+            if details:
+                raise ValidationError(_('CIFs are not allowed in this field'))
+            else:
+                raise ValidationError(_('CID %(cid)s is incorrect'), params={'cid': cid})
+        elif kind == 3 and not nie:
+            if details:
+                raise ValidationError(_('NIEs are not allowed in this field'))
+            else:
+                raise ValidationError(_('CID %(cid)s is incorrect'), params={'cid': cid})
+
+    # Return function
+    return lambda cid: worker(cid, nif, nie, cif, details)
+
+
+# Alias as necesary for migrate to work
+def spanishNIFNIE(cid):
+    func = spanishNIFNIECIF(cif=False)
+    func(cid)
 
 
 if __name__ == "__main__":
-    print(spanishNIFNIECIF('12345678Z'))
+
+    nifs = []
+    nifs.append(('12345678Z', '12344678Z'))
+    cifs = []
+    cifs.append(('A58818501', 'A58813501'))
+    cifs.append(('Q3638496D', 'Q3638496C'))
+    nies = []
+    nies.append(('Z3698658X', 'Z3698458X'))
+    invalids = []
+    invalids.append(('1234567890Z', -4, 'INCORRECT'))
+    invalids.append(('A12X4567Z', -5, 'INCORRECT'))
+    invalids.append(('O12345678', -6, 'INCORRECT'))
+
+    idx = 1
+    tests = []
+    for (ts, r, k) in [(nifs, 1, 'NIF'), (cifs, 2, 'CIF'), (nies, 3, 'NIE')]:
+        for (v, i) in ts:
+            tests.append((v, r, k))
+            tests.append((i, -r, k))
+    for (t, r, k) in invalids:
+        tests.append((t, r, k))
+
+    for (value, result, kind) in tests:
+        res = spanishNIFNIECIFhelper(value)
+        assert res == result, "TestA {}: {} {} detection if failing, expected {} and got {}".format(idx, kind, value, result, res)
+        print("Test A/{}: {} {} passed)".format(idx, kind, value))
+        idx += 1
+
+    idx = 1
+    func = spanishNIFNIECIF()
+    for (value, result, kind) in tests:
+        try:
+            func(value)
+            valid = True
+        except ValidationError as e:
+            valid = False
+
+        assert (valid == bool(result > 0)), "TestB {}: {} {} validation is failing, expected {} and got {}".format(idx, kind, value, bool(result > 0), valid)
+        print("Test B/{}: {} {} passed)".format(idx, kind, value))
+        idx += 1
+
+    idx = 1
+    for (value, result, kind) in tests:
+        try:
+            spanishNIFNIE(value)
+            valid = True
+        except ValidationError as e:
+            valid = False
+
+        assert (valid == bool((result > 0) and (kind != 'CIF'))), "TestC {}: {} {} validation is failing, expected {} and got {}".format(idx, kind, value, bool((result > 0) and (kind != 'CIF')), valid)
+        print("Test C/{}: {} {} passed)".format(idx, kind, value))
+        idx += 1
