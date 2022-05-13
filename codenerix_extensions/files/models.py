275443ede1a55a0eda10b671ef86b6d0f50b64df
Juan J Soler <soleronline@gmail.com>
2022-05-13 08:06:46 +0200

    Migrate to Django 4

--- codenerix_extensions/files/models.py
+++ codenerix_extensions/files/models.py
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 from codenerix.lib.helpers import upload_path
